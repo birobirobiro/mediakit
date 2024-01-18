41e64bb525cac598ac184e1ac65c48358f93b865
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-01-18 18:55:22 -0300

    beautify code

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
+const express = require("express");
+const puppeteer = require("puppeteer");
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
-})
+  console.log("Server started on port 3000");
+});
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const socialMediaData = await getSocialMediaData();
   res.send(socialMediaData);
-})
+});
 
 const getSocialMediaData = async () => {
   const browser = await puppeteer.launch({
-    headless: 'new',
+    headless: "new",
   });
   const page = await browser.newPage();
 
-
-  const user = 'birobirobiro';
+  const user = "birobirobiro";
   const url = `https://beacons.ai/${user}/mediakit`;
-  await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
   // Wait for the social media links to be present
   await page.waitForSelector('.mt-4 a[aria-label^="profile followers"]');
 
   const socialMediaData = await page.evaluate(() => {
-    const socialMediaLinks = document.querySelectorAll('.mt-4 a[aria-label^="profile followers"]');
-    const data = Array.from(socialMediaLinks, link => {
-      const title = link.getAttribute('aria-label').split(' ')[2];
-      const countText = link.querySelector('.font-bold').textContent.trim();
-      let count = parseFloat(countText.replace(/k/i, 'e3').replace(/m/i, 'e6').replace(/,/g, ''));
+    const socialMediaLinks = document.querySelectorAll(
+      '.mt-4 a[aria-label^="profile followers"]'
+    );
+    const data = Array.from(socialMediaLinks, (link) => {
+      const title = link.getAttribute("aria-label").split(" ")[2];
+      const countText = link.querySelector(".font-bold").textContent.trim();
+      let count = parseFloat(
+        countText.replace(/k/i, "e3").replace(/m/i, "e6").replace(/,/g, "")
+      );
       if (isNaN(count)) {
         count = countText; // If the count is not a number, keep it as text
       }
